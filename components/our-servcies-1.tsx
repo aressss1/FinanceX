aa498c911ebeccd636b0f3a4ee4c4da7b602c682
aressss1 <codewithfaizi@gmail.com>
2023-12-06 17:56:51 +0500

    f-

--- components/our-servcies-1.tsx
+++ components/our-servcies-1.tsx
@@ -55,17 +55,17 @@ const OurServices1 = () => {
             {servicesCardContent.map((card) => (
                 <Card
                      key={card.id} 
-                     className="group group-hover:bg-zinc-800 px-6 py-5 bg-black flex flex-col border border-zinc-800 rounded-none " >
-                    <CardHeader className="flex flex-col gap-4"  >
+                     className="group  px-6 py-5 bg-black flex flex-col border border-zinc-800 rounded-none " >
+                    <CardHeader className=" group-hover:bg-zinc-800 flex flex-col gap-4"  >
                         <Minus className=" text-white " />
                         <div className="w-64 text-white text-2xl font-medium ">
                             {card.title}
                         </div>
                     </CardHeader>
-                    <CardContent className="py-5 text-white text-opacity-70 text-base font-normal leading-normal" >
+                    <CardContent className=" group-hover:bg-zinc-800 py-5 text-white text-opacity-70 text-base font-normal leading-normal" >
                         {card.description}
                     </CardContent>
-                    <CardFooter className="flex pt-3 justify-end" >
+                    <CardFooter className=" flex pt-3 justify-end" >
                         <Button className="w-14 h-14 bg-white group-hover:bg-special " >
                             <MoveUpRight className="text-black" />
                         </Button>
