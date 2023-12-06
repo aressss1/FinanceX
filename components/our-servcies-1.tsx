faea51fe40a23d0a517d6999f4c36221f1c0c6dd
aressss1 <codewithfaizi@gmail.com>
2023-12-06 17:23:13 +0500

    f-

--- components/our-servcies-1.tsx
+++ components/our-servcies-1.tsx
@@ -1,20 +1,47 @@
 import { MoveRight } from "lucide-react";
 import { Button } from "./ui/button";
-import { Minus , MoveUpRight } from 'lucide-react';
+import { Minus, MoveUpRight } from 'lucide-react';
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
 
 
+
 const OurServices1 = () => {
+    const servicesCardContent = [
+        {
+            id: 1,
+            title: "Checking accounts",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus."
+        },
+        {
+            id: 2,
+            title: "Checking accounts",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus."
+        },
+        {
+            id: 3,
+            title: "Checking accounts",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus."
+        },
+        {
+            id: 4,
+            title: "Checking accounts",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus."
+        },
+        {
+            id: 5,
+            title: "Checking accounts",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus."
+        },
+    ]
+
     return (
         <div className="px-20 py-20 bg-black grid grid-cols-3 ">
-            <div className=" col-span-2 flex flex-col gap-6" >
+            <div className="flex flex-col gap-6" >
                 <div className="text-special text-xl font-semibold ">
                     Our Services
                 </div>
@@ -25,38 +52,26 @@ const OurServices1 = () => {
                     <MoveRight />
                 </Button>
             </div>
-            <Card className="px-6 py-5 bg-black flex flex-col border border-zinc-800" >
-                <CardHeader className="flex flex-col gap-4"  >
-                    <Minus className=" text-white " />
-                    <div className="w-64 text-white text-2xl font-medium ">
-                        Checking accounts
-                    </div>
-                </CardHeader>
-                <CardContent className="py-5 text-white text-opacity-70 text-base font-normal leading-normal" >
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus.
-                </CardContent>
-                <CardFooter className="flex pt-3 justify-end" >
-                    <Button className="w-14 h-14 bg-white " >
-                        <MoveUpRight className="text-black"  />
-                    </Button>
-                </CardFooter>
-            </Card>
-            <Card className="px-6 py-5 bg-black flex flex-col border border-zinc-800" >
-                <CardHeader className="flex flex-col gap-4"  >
-                    <Minus className=" text-white " />
-                    <div className="w-64 text-white text-2xl font-medium ">
-                        Checking accounts
-                    </div>
-                </CardHeader>
-                <CardContent className="py-5 text-white text-opacity-70 text-base font-normal leading-normal" >
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ullam eaque! Nihil mollitia tempore laborum harum cum doloribus eligendi itaque ad quisquam, et expedita libero vitae consectetur? Mollitia, illo natus.
-                </CardContent>
-                <CardFooter className="flex pt-3 justify-end" >
-                    <Button className="w-14 h-14 bg-white " >
-                        <MoveUpRight className="text-black"  />
-                    </Button>
-                </CardFooter>
-            </Card>
+            {servicesCardContent.map((card) => (
+                <Card key={card.id} className="px-6 py-5 bg-black flex flex-col border border-zinc-800" >
+                    <CardHeader className="flex flex-col gap-4"  >
+                        <Minus className=" text-white " />
+                        <div className="w-64 text-white text-2xl font-medium ">
+                            {card.title}
+                        </div>
+                    </CardHeader>
+                    <CardContent className="py-5 text-white text-opacity-70 text-base font-normal leading-normal" >
+                        {card.description}
+                    </CardContent>
+                    <CardFooter className="flex pt-3 justify-end" >
+                        <Button className="w-14 h-14 bg-white " >
+                            <MoveUpRight className="text-black" />
+                        </Button>
+                    </CardFooter>
+                </Card>
+            ))}
+
+
         </div>
     );
 }
