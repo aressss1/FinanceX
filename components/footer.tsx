2dbea521eee711cb6dc43108eb63cf4436f209e3
aressss1 <codewithfaizi@gmail.com>
2023-12-07 17:50:13 +0500

    f-

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import { Separator } from './ui/separator';
 const Footer = () => {
     return (
         <div className="bg-black" >
-            <div className="px-20 py-12 grid grid-rows-4 gap-3" >
+            <div className="px-20 py-14 grid grid-cols-4 gap-1" >
                 <div className="flex flex-col gap-4" >
                     <div className="text-white text-xl font-bold">
                         FinanceX
@@ -14,7 +14,7 @@ const Footer = () => {
                     <div className="w-52 text-white text-opacity-70 text-base font-normal leading-normal">
                         Lorem ipsum dolor sit amet, consectetur
                     </div>
-                    <div className="flex gap-3 justify-center items-center text-white " >
+                    <div className="flex gap-5 justify-start items-center text-white " >
                         <Linkedin className='h-5 w-5' />
                         <Instagram className='h-5 w-5' />
                         <Twitter className='h-5 w-5' />
@@ -54,12 +54,12 @@ const Footer = () => {
                         <Button className='bg-special rounded-none text-black ' type="submit">Subscribe</Button>
                     </div>
                     <div className="opacity-50 text-white text-opacity-70 text-base font-normal leading-relaxed">
-                        *  Will send you weekly updates for your better<br />finance management.
+                        *  Will send you weekly updates for your better finance management.
                     </div>
                 </div>
             </div>
-            <Separator  className='h-[1px] text-white text-opacity-70 '/>
-            <div className="text-center text-white text-opacity-70 text-base font-medium ">
+            <Separator  className=' text-white text-opacity-70 '/>
+            <div className="p-3 text-center text-white text-opacity-70 text-base font-medium ">
                 Copyright &copy; FinanceX 2022. All Rights Reserved.
             </div>
         </div>
