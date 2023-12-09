e46e0a0dc2e5f5bc94868765920555abce58c7a4
aressss1 <codewithfaizi@gmail.com>
2023-12-09 10:54:48 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -4,15 +4,37 @@ import { Card, CardContent, CardFooter } from './ui/card';
 import Image from 'next/image';
 
 const Testimonials = () => {
+    const reviews = [
+        {
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel",
+            imgSrc: "client/client-1.svg",
+            name: "Ranveer",
+            role: "Webflow Developer"
+        },
+        {
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel",
+            imgSrc: "client/client-2.svg",
+            name: "Tony",
+            role: "Webflow Developer"
+        },
+        {
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel",
+            imgSrc: "client/client-3.svg",
+            name: "Alex",
+            role: "Webflow Developer"
+        },
+    ]
+
+
     return (
         <div className="bg-special py-20" >
             <div className="px-20 flex flex-row justify-between items-center " >
-                <div className="py-4 flex flex-col gap-2 w-96" >
+                <div className="py-4 flex flex-col gap-2" >
                     <div className="text-neutral-900 text-xl font-semibold ">
                         Our Services
                     </div>
-                    <div className=" text-neutral-900 text-7xl font-bold font-['Satoshi'] leading-10">
-                        Global businesses love FinanceX
+                    <div className=" text-neutral-900 text-7xl font-bold  leading-[3.5rem]">
+                        Global businesses <br /> love FinanceX
                     </div>
                 </div>
                 <div className='flex flex-row items-center gap-5 justify-end' >
@@ -25,26 +47,29 @@ const Testimonials = () => {
                 </div>
             </div>
             <div className='flex items-center justify-center'>
-                <Card className="bg-black flex flex-col  rounded-none p-10 gap-5 " >
-                    <CardContent className=" text-white text-sm font-normal leading-loose" >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel
-                    </CardContent>
-                    <CardFooter className="flex flex-row justify-start gap-4 " >
-                        <Image
-                            className='w-12 h-12  rounded-full'
-                            src="client/client-1.svg"
-                            alt='client'
-                        />
-                        <div className='flex flex-col gap-2' >
-                            <div className="text-white text-sm font-bold ">
-                                Ranveer
+                {reviews.map((review) => (
+                    <Card key={review.name} className="bg-black flex flex-col  rounded-none p-10 gap-5 " >
+                        <CardContent className=" text-white text-sm font-normal leading-loose" >
+                            {review.review}
+                        </CardContent>
+                        <CardFooter className="flex flex-row justify-start gap-4 " >
+                            <Image
+                                className='w-12 h-12  rounded-full'
+                                src={review.imgSrc}
+                                alt='client'
+                            />
+                            <div className='flex flex-col gap-2' >
+                                <div className="text-white text-sm font-bold ">
+                                    {review.name}
+                                </div>
+                                <div className="text-white text-opacity-70 text-xs font-normal ">
+                                    {review.role}
+                                </div>
                             </div>
-                            <div className="text-white text-opacity-70 text-xs font-normal ">
-                                Webflow Developer
-                            </div>
-                        </div>
-                    </CardFooter>
-                </Card>
+                        </CardFooter>
+                    </Card>
+
+                ))}
             </div>
         </div>
     );
