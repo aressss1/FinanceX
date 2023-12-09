2824bde9c8a0e7a4714e03652b8de6ae5494fd41
aressss1 <codewithfaizi@gmail.com>
2023-12-09 11:26:10 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -32,35 +32,35 @@ const Testimonials = () => {
 
 
     return (
-        <Swiper
-            loop={true}
-            modules={[Navigation]}
-            slidesPerView={1}
-            navigation
-        >
-            <div className="bg-special py-20" >
-                <div className="px-20 pb-14 flex flex-row justify-between items-center " >
-                    <div className=" flex flex-col gap-5" >
-                        <div className="text-neutral-900 text-xl font-semibold ">
-                            Our Services
-                        </div>
-                        <div className=" text-neutral-900 text-7xl font-bold  leading-[4.5rem]">
-                            Global businesses <br /> love FinanceX
-                        </div>
+        <div className="bg-special py-20" >
+            <div className="px-20 pb-14 flex flex-row justify-between items-center " >
+                <div className=" flex flex-col gap-5" >
+                    <div className="text-neutral-900 text-xl font-semibold ">
+                        Our Services
                     </div>
-                    <div className='flex flex-row items-center gap-5 justify-end' >
-                        <Button className='w-14 h-14 rounded-full bg-special border border-black ' >
-                            <ArrowLeft className='text-black' />
-                        </Button>
-                        <Button className='w-14 h-14 rounded-full bg-black  ' >
-                            <ArrowRight className='text-white' />
-                        </Button>
+                    <div className=" text-neutral-900 text-7xl font-bold  leading-[4.5rem]">
+                        Global businesses <br /> love FinanceX
                     </div>
                 </div>
+                <div className='flex flex-row items-center gap-5 justify-end' >
+                    <Button className='w-14 h-14 rounded-full bg-special border border-black ' >
+                        <ArrowLeft className='text-black' />
+                    </Button>
+                    <Button className='w-14 h-14 rounded-full bg-black  ' >
+                        <ArrowRight className='text-white' />
+                    </Button>
+                </div>
+            </div>
+            <Swiper
+                loop={true}
+                modules={[Navigation]}
+                slidesPerView={1}
+                navigation
+            >
                 <div className='flex items-center justify-center gap-6 pl-7'>
-                    {reviews.map((review) => (
-                        <SwiperSlide key={review.name} >
-                            <Card  className="bg-black border-none flex flex-col  rounded-none p-10 gap-5 " >
+                    <SwiperSlide  >
+                        {reviews.map((review) => (
+                            <Card key={review.name} className="bg-black border-none flex flex-col  rounded-none p-10 gap-5 " >
                                 <CardContent className=" text-white text-sm font-normal leading-loose" >
                                     {review.review}
                                 </CardContent>
@@ -80,11 +80,11 @@ const Testimonials = () => {
                                     </div>
                                 </CardFooter>
                             </Card>
-                        </SwiperSlide>
-                    ))}
+                        ))}
+                    </SwiperSlide>
                 </div>
-            </div>
-        </Swiper>
+            </Swiper>
+        </div>
     );
 }
 
