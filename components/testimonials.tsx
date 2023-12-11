4d45592fdad1b54fc209cb5f04cefdfc9e87046c
aressss1 <codewithfaizi@gmail.com>
2023-12-11 11:25:24 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -4,21 +4,20 @@ import { ArrowRight, ArrowLeft } from 'lucide-react';
 import { Button } from './ui/button';
 import { Card, CardContent, CardFooter } from './ui/card';
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { useSwiper } from 'swiper/react';
-import 'swiper/css';
+import { useState } from 'react';
 
 const Testimonials = () => {
-    const swiper = useSwiper()
+    const [startIndex, setStartIndex] = useState(0);
 
-    const prevClick = () => {
-        
-    }
+    const handleNext = () => {
+        const newStartIndex = Math.min(startIndex + 1, reviews.length - 3);
+        setStartIndex(newStartIndex);
+    };
 
-    const nextClick = () => {
-
-    }
+    const handlePrev = () => {
+        const newStartIndex = Math.max(startIndex - 1, 0);
+        setStartIndex(newStartIndex);
+    };
 
     const reviews = [
         {
@@ -45,6 +44,12 @@ const Testimonials = () => {
             name: "Sammy",
             role: "Webflow Developer"
         },
+        {
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel",
+            imgSrc: "client/client-3.svg",
+            name: "Alferdo",
+            role: "Webflow Developer"
+        },
     ]
 
 
@@ -60,55 +65,44 @@ const Testimonials = () => {
                     </div>
                 </div>
             </div>
-            <Swiper
-                // install Swiper modules
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={50}
-                slidesPerView={3}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                className='pl-7'
-            >
-                <div className='flex flex-row items-center gap-5 justify-end' >
-                    <Button 
-                         
-                        className='w-14 h-14 rounded-full bg-special border border-black ' >
-                            <ArrowLeft onClick={(e) => swiper.slidePrev()} className='text-black' />
-                    </Button>
-                    <Button 
-                        onClick={(e) => swiper.slideNext()}
-                        className='w-14 h-14 rounded-full bg-black  ' >
-                        <ArrowRight className='text-white' />
-                    </Button>
-                </div>
-                <div className='flex items-center justify-center gap-6 '>
-                    {reviews.map((review) => (
-                        <SwiperSlide key={review.name} className='pl-7' >
-                            <Card  className="bg-black w-96 border-none flex flex-col  rounded-none p-10 gap-5 " >
-                                <CardContent className=" text-white text-sm font-normal leading-loose" >
-                                    {review.review}
-                                </CardContent>
-                                <CardFooter className="flex flex-row justify-start gap-4 " >
-                                    <Image
-                                        className='w-12 h-12  rounded-full'
-                                        src={review.imgSrc}
-                                        alt='client'
-                                    />
-                                    <div className='flex flex-col gap-2' >
-                                        <div className="text-white text-sm font-bold ">
-                                            {review.name}
-                                        </div>
-                                        <div className="text-white text-opacity-70 text-xs font-normal ">
-                                            {review.role}
-                                        </div>
-                                    </div>
-                                </CardFooter>
-                            </Card>
-                        </SwiperSlide>
-                    ))}
-                </div>
-            </Swiper>
+            <div className='flex flex-row items-center gap-5 justify-end' >
+                <Button
+                    onClick={handlePrev}
+                    disabled={startIndex === 0}
+                    className='w-14 h-14 rounded-full bg-special border border-black ' >
+                    <ArrowLeft className='text-black' />
+                </Button>
+                <Button
+                    onClick={handleNext}
+                    disabled={startIndex === reviews.length - 3}
+                    className='w-14 h-14 rounded-full bg-black  ' >
+                    <ArrowRight className='text-white' />
+                </Button>
+            </div>
+            <div className='flex items-center justify-center gap-6 '>
+                {reviews.slice(startIndex, startIndex + 3).map((review, index) => (
+                    <Card key={review.name} className="bg-black w-96 border-none flex flex-col  rounded-none p-10 gap-5 " >
+                        <CardContent className=" text-white text-sm font-normal leading-loose" >
+                            {review.review}
+                        </CardContent>
+                        <CardFooter className="flex flex-row justify-start gap-4 " >
+                            <Image
+                                className='w-12 h-12  rounded-full'
+                                src={review.imgSrc}
+                                alt='client'
+                            />
+                            <div className='flex flex-col gap-2' >
+                                <div className="text-white text-sm font-bold ">
+                                    {review.name}
+                                </div>
+                                <div className="text-white text-opacity-70 text-xs font-normal ">
+                                    {review.role}
+                                </div>
+                            </div>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 }
