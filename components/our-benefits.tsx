1204cf7fc133ce4a0d7d0fa56dced6b5cc165a9d
aressss1 <codewithfaizi@gmail.com>
2023-12-12 16:37:17 +0500

    f-

--- components/our-benefits.tsx
+++ components/our-benefits.tsx
@@ -4,43 +4,43 @@ import Image from 'next/image';
 
 const OurBenefits = () => {
     return ( 
-        <div className="flex flex-row justify-between lg:px-20 py-12 lg:gap-3 md:px-12 md:gap-6 bg-black" >
-            <div className="w-[28rem] flex flex-col gap-8" >
-            <div className="text-special lg:text-xl md:text-base font-semibold">
+        <div className="flex flex-row justify-between px-2 lg:px-20 py-12 lg:gap-3 md:px-12 md:gap-6 bg-black" >
+            <div className="w-72 md:w-[28rem] flex flex-col gap-8" >
+            <div className="text-special text-xs lg:text-xl md:text-base font-semibold">
                 Benefits
             </div>
             <div>
-                <span className="text-white lg:text-5xl md:text-4xl font-bold leading-10">We have many users all over the </span><span className="text-special lg:text-5xl  md:text-4xl font-bold leading-10">world</span>
+                <span className="text-white text-xl lg:text-5xl md:text-4xl font-bold leading-10">We have many users all over the </span><span className="text-special  text-xl lg:text-5xl  md:text-4xl font-bold leading-10">world</span>
             </div>
-            <div className=" text-white text-opacity-70 text-base font-normal leading-normal">
+            <div className=" text-white text-opacity-70 text-xs md:text-base font-normal leading-normal">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna 
             </div>
             <div className='flex flex-row justify-between' >
                 <div className='flex flex-col justify-between gap-3 ' >
-                    <div className='flex flex-row items-center lg:text-3xl md:text-2xl text-white uppercase ' >
+                    <div className='flex flex-row items-center text-xl lg:text-3xl md:text-2xl text-white uppercase ' >
                         10M
                         <Plus className='text-special'  />
                     </div>
-                    <div className="text-special text-sm font-normal leading-none">
+                    <div className="text-special text-xs md:text-sm font-normal leading-none">
                         User active
                     </div>
                 </div>
                 <div className='flex flex-col justify-between gap-3 ' >
-                    <div className='flex flex-row items-center  lg:text-3xl md:text-2xl text-white uppercase ' >
+                    <div className='flex flex-row items-center text-xl lg:text-3xl md:text-2xl text-white uppercase ' >
                         30
                         <Plus className='text-special'  />
                     </div>
-                    <div className="text-special text-sm font-normal leading-none">
+                    <div className="text-special text-xs md:text-sm font-normal leading-none">
                         Country
                     </div>
                 </div>
                 <div className='flex flex-col justify-between gap-3 ' >
-                    <div className='flex flex-row items-center lg:text-3xl md:text-2xl text-white uppercase ' >
+                    <div className='flex flex-row items-center text-xl lg:text-3xl md:text-2xl text-white uppercase ' >
                         <DollarSign className='h-9' />
                         50M
                         <Plus className='text-special'  />
                     </div>
-                    <div className="text-special text-sm font-normal leading-none">
+                    <div className="text-special text-xs md:text-sm font-normal leading-none">
                         Transactions
                     </div>
                 </div>
