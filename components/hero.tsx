9c435be92989cc891ab1528ae1dd7b99e9e90d8a
aressss1 <codewithfaizi@gmail.com>
2023-12-12 16:13:21 +0500

    f-

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,13 +6,13 @@ const HeroSection = () => {
     return (
         <div className="h-[37rem] flex bg-black items-center justify-between" >
             <div className="w-[38rem] flex flex-col lg:pl-20 md:pl-12 gap-5  " >
-                <div className="text-special text-xl font-semibold">
+                <div className="text-special text-base md:text-xl font-semibold">
                     Welcome! Future Platform
                 </div>
                 <div >
-                    <span className="text-white lg:text-7xl md:text-5xl font-black">The World&apos;s Most Modern Card </span><span className="text-special lg:text-7xl md:text-5xl font-black">Platform</span>
+                    <span className="text-white text-xl lg:text-7xl md:text-5xl font-black">The World&apos;s Most Modern Card </span><span className="text-special text-xl lg:text-7xl md:text-5xl font-black">Platform</span>
                 </div>
-                <div className=" text-white text-opacity-70 lg:text-lg md:text-base font-normal leading-relaxed">
+                <div className=" text-white text-opacity-70 text-sm lg:text-lg md:text-base font-normal leading-relaxed">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor
                 </div>
                 <Button className="lg:w-56 lg:h-16 md:w-40 md:h-12 lg:px-11 lg:py-5 md:px-8 md:py-4 md:text-xs md:gap-3  lg:text-sm  bg-special rounded-none hover:bg-lime-600 text-black items-center lg:gap-5 inline-flex" >
