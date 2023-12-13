5a06d8f6f102bc9f36c7ed6dd97d721635946e60
aressss1 <codewithfaizi@gmail.com>
2023-12-13 14:32:18 +0500

    f-

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,26 +6,26 @@ import { Separator } from './ui/separator';
 const Footer = () => {
     return (
         <div className="bg-black" >
-            <div className="lg:px-20 py-14 md:px-12 grid grid-cols-4 gap-1" >
-                <div className="flex flex-col lg:gap-4 md:gap-2 " >
-                    <div className="text-white lg:text-xl md:text-base font-bold">
+            <div className="lg:px-20 py-14 md:px-12 px-2 grid grid-cols-2 md:grid-cols-4 gap-1" >
+                <div className="flex flex-col lg:gap-4 gap-3 " >
+                    <div className="text-white lg:text-xl md:text-base text-sm font-bold">
                         FinanceX
                     </div>
-                    <div className="lg:w-52 md:w-40 text-white text-opacity-70 lg:text-base md:text-sm font-normal leading-normal">
+                    <div className="lg:w-52 md:w-40 text-white text-opacity-70 lg:text-base md:text-sm text-xs font-normal leading-normal">
                         Lorem ipsum dolor sit amet, consectetur
                     </div>
                     <div className="flex gap-5 justify-start items-center text-white " >
-                        <Linkedin className='lg:h-5 lg:w-5 md:h-4 md:w-4' />
-                        <Instagram className='lg:h-5 lg:w-5 md:h-4 md:w-4 ' />
-                        <Twitter className='lg:h-5 lg:w-5 md:h-4 md:w-4 ' />
+                        <Linkedin className='lg:h-5 lg:w-5 h-4 w-4' />
+                        <Instagram className='lg:h-5 lg:w-5 h-4 w-4 ' />
+                        <Twitter className='lg:h-5 lg:w-5 h-4 w-4 ' />
                     </div>
                 </div>
 
-                <div className='flex flex-col lg:gap-4 md:gap-2' >
-                    <div className="text-white text-opacity-70 lg:text-xl md:text-base font-bold">
+                <div className='flex flex-col lg:gap-4 gap-3' >
+                    <div className="text-white text-opacity-70 lg:text-xl md:text-base text-sm font-bold">
                         Company
                     </div>
-                    <div className="text-white flex flex-col gap-4 text-opacity-70 lg:text-base md:text-sm font-normal leading-normal">
+                    <div className="text-white flex flex-col gap-4 text-opacity-70 lg:text-base md:text-sm text-sm font-normal leading-normal">
                         <p>About Us</p>
                         <p>Careers</p>
                         <p>Blog</p>
@@ -33,11 +33,11 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col lg:gap-4 md:gap-2' >
-                    <div className="text-white text-opacity-70 lg:text-xl md:text-base font-bold">
+                <div className='flex flex-col lg:gap-4 gap-3' >
+                    <div className="text-white text-opacity-70 lg:text-xl md:text-base text-sm font-bold">
                         Resources
                     </div>
-                    <div className="text-white flex flex-col gap-4 text-opacity-70 lg:text-base md:text-sm font-normal leading-normal">
+                    <div className="text-white flex flex-col gap-4 text-opacity-70 lg:text-base md:text-sm text-sm font-normal leading-normal">
                         <p>Templates</p>
                         <p>Tutorials</p>
                         <p>Free Resources</p>
@@ -45,15 +45,15 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col lg:gap-4 md:gap-2' >
-                    <div className="text-white text-opacity-70 lg:text-xl md:text-base font-bold  leading-normal">
+                <div className='flex flex-col lg:gap-4 gap-3' >
+                    <div className="text-white text-opacity-70 lg:text-xl md:text-base text-sm font-bold  leading-normal">
                         Join Our Newsletter
                     </div>
                     <div className="flex w-full max-w-sm items-center">
-                        <Input className='rounded-none bg-white bg-opacity-70 md:text-xs lg:text-sm' type="email" placeholder="Your email address" />
+                        <Input className='rounded-none bg-white bg-opacity-70 text-xs lg:text-sm' type="email" placeholder="Your email address" />
                         <Button className='bg-special rounded-none text-black lg:w-20 md:w-16 md:text-xs md:px-2 md:py-1 ' type="submit">Subscribe</Button>
                     </div>
-                    <div className="opacity-50 text-white text-opacity-70 lg:text-base md:text-sm font-normal leading-relaxed">
+                    <div className="opacity-50 text-white text-opacity-70 lg:text-base md:text-sm text-xs font-normal leading-relaxed">
                         *  Will send you weekly updates for your better finance management.
                     </div>
                 </div>
