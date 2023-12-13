d1c87a2b1dd9fa17ce1d71b86b2632983f6f9f86
aressss1 <codewithfaizi@gmail.com>
2023-12-13 14:23:17 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,23 +1,23 @@
-// 'use client'
+'use client'
 
 import { ArrowRight, ArrowLeft } from 'lucide-react';
 import { Button } from './ui/button';
 import { Card, CardContent, CardFooter } from './ui/card';
 import Image from 'next/image';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const Testimonials = () => {
-    // const [startIndex, setStartIndex] = useState(0);
+    const [startIndex, setStartIndex] = useState(0);
 
-    // const handleNext = () => {
-    //     const newStartIndex = Math.min(startIndex + 1, reviews.length - 3);
-    //     setStartIndex(newStartIndex);
-    // };
+    const handleNext = () => {
+        const newStartIndex = Math.min(startIndex + 1, reviews.length - 3);
+        setStartIndex(newStartIndex);
+    };
 
-    // const handlePrev = () => {
-    //     const newStartIndex = Math.max(startIndex - 1, 0);
-    //     setStartIndex(newStartIndex);
-    // };
+    const handlePrev = () => {
+        const newStartIndex = Math.max(startIndex - 1, 0);
+        setStartIndex(newStartIndex);
+    };
 
     const reviews = [
         {
@@ -64,7 +64,7 @@ const Testimonials = () => {
                         Global businesses <br /> love FinanceX
                     </div>
                 </div>
-                {/* <div className='hidden  md:flex flex-row items-center gap-5 justify-end' >
+                <div className='hidden  md:flex flex-row items-center gap-5 justify-end' >
                     <Button
                         onClick={handlePrev}
                         disabled={startIndex === 0}
@@ -77,11 +77,11 @@ const Testimonials = () => {
                         className='w-14 h-14 rounded-full bg-black  ' >
                         <ArrowRight className='text-white' />
                     </Button>
-                </div> */}
+                </div>
             </div>
 
             {/* for MID & LARGE devices */}
-            {/* <div className='hidden md:flex items-center overflow-x-auto no-scrollbar justify-center gap-6 '>
+            <div className='hidden md:flex items-center overflow-x-auto no-scrollbar justify-center gap-6 '>
                 {reviews.slice(startIndex, startIndex + 3).map((review, index) => (
                     <Card key={review.name} className="bg-black lg:w-96 w-56 border-none flex flex-col  rounded-none lg:p-10 md:p-2 p-2 lg:gap-5 md:gap-4 transition-all duration-500 transform hover:scale-105 " >
                         <CardContent className=" text-white text-sm lg:p-6 md:p-4 p-2 md:pt-1 font-normal  lg:leading-loose md:leading-relaxed" >
@@ -104,13 +104,13 @@ const Testimonials = () => {
                         </CardFooter>
                     </Card>
                 ))}
-            </div> */}
+            </div>
 
             {/* for Small Devices */}
-            <div className=' md:hidden flex flex-row overflow-x-auto no-scrollbar  gap-6 '>
+            <div className=' md:hidden flex flex-row overflow-x-auto no-scrollbar  gap-6 px-2'>
                 {reviews.map((review) => (
-                    <Card key={review.name} className="bg-black w-52 border-none flex flex-col  rounded-none  p-2 " >
-                        <CardContent className=" text-white text-sm p-2 font-normal" >
+                    <Card key={review.name} className="bg-black  h-[263px] border-none flex flex-col  rounded-none  p-2 " >
+                        <CardContent className="  text-white w-52 text-sm p-2 font-normal" >
                             {review.review}
                         </CardContent>
                         <CardFooter className="flex flex-row p-2 justify-start gap-4 " >
