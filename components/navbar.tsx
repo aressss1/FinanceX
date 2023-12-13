afe4e2e3db9185928dc49ba825f1df63eac31356
aressss1 <codewithfaizi@gmail.com>
2023-12-13 15:14:41 +0500

    f-

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,18 @@
 import { Menu, MoveRight } from 'lucide-react';
 import { Button } from './ui/button';
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 const Navbar = () => {
-    return ( 
+    return (
         <div className="h-[68px] bg-black flex flex-row justify-between items-center px-2 py-2 lg:px-20 md:px-12" >
             <div className="text-white text-xl font-bold">
                 FinanceX
@@ -27,11 +37,41 @@ const Navbar = () => {
                 SignUp
                 <MoveRight />
             </Button>
-            <Button className="md:hidden  h-10 items-center justify-center rounded-none bg-special hover:bg-lime-600 text-black  " >
-                <Menu />
-            </Button>
+
+            {/* Responive Navbar / Hamburger-Menu */}
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button className="md:hidden  h-10 items-center justify-center rounded-none bg-special hover:bg-lime-600 text-black  " >
+                        <Menu />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <div className='flex flex-col gap-4 ' >
+                        <ul className="text-white text-opacity-70  text-base font-normal flex flex-col gap-10 " >
+                            <li>
+                                why FinanceX
+                            </li>
+                            <li>
+                                Features
+                            </li>
+                            <li>
+                                Updates
+                            </li>
+                            <li>
+                                Blog
+                            </li>
+                        </ul>
+                        <Button className="hidden  md:flex flex-row gap-2 items-center justify-center rounded-none bg-special hover:bg-lime-600 text-black  " >
+                            SignUp
+                            <MoveRight />
+                        </Button>
+                    </div>
+
+                </SheetContent>
+            </Sheet>
+
         </div>
-     );
+    );
 }
- 
+
 export default Navbar;
