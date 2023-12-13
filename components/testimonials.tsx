5483df4cfbe03950f80e333bfdb6dd827c9d2dc6
aressss1 <codewithfaizi@gmail.com>
2023-12-13 11:28:14 +0500

    f-

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -55,16 +55,16 @@ const Testimonials = () => {
 
     return (
         <div className="bg-special py-20  " >
-            <div className="lg:px-20 md:px-12 pb-14 flex flex-row justify-between items-center " >
+            <div className="lg:px-20 md:px-12 px-2 pb-14 flex flex-row justify-between items-center " >
                 <div className=" flex flex-col gap-5" >
-                    <div className="text-neutral-900 lg:text-xl md:text-base font-semibold ">
+                    <div className="text-neutral-900 lg:text-xl md:text-base text-xs font-semibold ">
                         Our Services
                     </div>
-                    <div className=" text-neutral-900 lg:text-7xl md:text-5xl font-bold  lg:leading-[4.5rem] md:leading-[3.25rem]">
+                    <div className=" text-neutral-900 lg:text-7xl md:text-5xl text-xl font-bold  lg:leading-[4.5rem] md:leading-[3.25rem] leading-8 ">
                         Global businesses <br /> love FinanceX
                     </div>
                 </div>
-                <div className='flex flex-row items-center gap-5 justify-end' >
+                <div className='hidden  md:flex flex-row items-center gap-5 justify-end' >
                 <Button
                     onClick={handlePrev}
                     disabled={startIndex === 0}
@@ -80,15 +80,15 @@ const Testimonials = () => {
             </div>
             </div>
             
-            <div className='flex items-center justify-center gap-6 '>
+            <div className='flex items-center overflow-x-auto no-scrollbar justify-center gap-6 '>
                 {reviews.slice(startIndex, startIndex + 3).map((review, index) => (
-                    <Card key={review.name} className="bg-black lg:w-96 md:w-56 border-none flex flex-col  rounded-none lg:p-10 md:p-2 lg:gap-5 md:gap-4 transition-all duration-500 transform hover:scale-105 " >
-                        <CardContent className=" text-white text-sm lg:p-6 md:p-4 md:pt-1 font-normal  lg:leading-loose md:leading-relaxed" >
+                    <Card key={review.name} className="bg-black lg:w-96 md:w-56 border-none flex flex-col  rounded-none lg:p-10 md:p-2 p-2 lg:gap-5 md:gap-4 transition-all duration-500 transform hover:scale-105 " >
+                        <CardContent className=" text-white text-sm lg:p-6 md:p-4 p-2 md:pt-1 font-normal  lg:leading-loose md:leading-relaxed" >
                             {review.review}
                         </CardContent>
-                        <CardFooter className="flex flex-row lg:p-6 md:p-4 justify-start gap-4 " >
+                        <CardFooter className="flex flex-row lg:p-6 md:p-4 p-2 justify-start gap-4 " >
                             <Image
-                                className='w-12 h-12  rounded-full'
+                                className='w-10 h-10  md:w-12 md:h-12  rounded-full'
                                 src={review.imgSrc}
                                 alt='client'
                             />
